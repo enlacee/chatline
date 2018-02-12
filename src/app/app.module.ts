aaf9f021ee450956d38e369d49b37c83dc03465d
Anibal Copitan <github@pprios.com>
2018-02-12 08:03:33 -0500

    [add] url protegida authGuard to access /admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { routes } from './app.routes';
-
+// import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { AdminComponent } from './admin/admin.component';
+// Guard Routes
+import { AuthguardGuard } from  './authguard.guard'; // add into module for use into
+import { UserService } from './user.service';
 
+const appRoutes: Routes = [
+	{
+		path: '',
+		component: LoginFormComponent,
+		pathMatch: 'full'
+	},
+	{
+		path: 'login',
+		component: LoginFormComponent,
+		pathMatch: 'full'
+	},
+	{
+		path: 'admin',
+		canActivate: [AuthguardGuard],
+		component: AdminComponent,
+		pathMatch: 'full'
+	},
+];
 
 @NgModule({
 	declarations: [
@@ -23,9 +44,10 @@ import { AdminComponent } from './admin/admin.component';
 		BrowserModule,
 		// import HttpClientModule after BrowserModule.
 		HttpClientModule,
-		routes
+		// HttpModule,
+		RouterModule.forRoot(appRoutes, {useHash: false, enableTracing: true})
 	],
-	providers: [],
+	providers: [AuthguardGuard],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authguard.guard.ts
@@ -0,0 +1,46 @@
+// import { Injectable } from '@angular/core';
+// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import { Observable } from 'rxjs/Observable';
+// import { UserService } from './user.service';
+// import { Router } from '@angular/router';
+
+// @Injectable()
+// export class AuthguardGuard implements CanActivate {
+
+// 	constructor(private user: UserService, private router: Router){}
+
+// 	canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+// 		this.router.navigate(['/']);
+// 		return this.user.getUserLoggedIn();
+// 	}
+// }
+
+
+// @Injectable()
+// export class AuthguardGuard implements CanActivate {
+
+// 	canActivate() {
+// 		console.log('AuthGuard#canActivate called');
+// 		return true;
+// 	}
+// }
+
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthguardGuard implements CanActivate {
+    constructor(private router: Router) {}
+
+    canActivate() {
+        if (localStorage.getItem('isLoggedin')) {
+            return true;
+        }
+
+        this.router.navigate(['/login']);
+        return false;
+    }
+}
+
+
